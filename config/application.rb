7fbcb6dfa0022a2d70840b3b2833263403aab31c
Filip Stybel <filip.stybel@gmail.com>
2015-02-01 13:21:07 +0100

    disable autogenerate css and coffee

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module IndividualOffer
   class Application < Rails::Application
     config.generators.test_framework false
-
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
