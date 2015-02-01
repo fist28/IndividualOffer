b93f5a27a0098c52504beb3d65ed7da74f9fc77a
Filip Stybel <filip.stybel@gmail.com>
2015-02-01 17:12:08 +0100

    Devise

--- app/models/company.rb
+++ app/models/company.rb
@@ -13,12 +13,13 @@
 #  company_id  :integer
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  user_id     :integer
 #
 
 class Company < ActiveRecord::Base
   has_many :clients, :class_name => 'Company', :dependent => :destroy, :foreign_key => :company_id
   belongs_to :parent, :class_name => 'Company', :foreign_key => :company_id
 
-
   has_many :offers
+  belongs_to :user
 end
